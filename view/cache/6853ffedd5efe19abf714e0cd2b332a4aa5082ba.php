0e895f60884c8d359de5a69adba0906fa0d97091
Muhereza Joel <muherezajoel40@gmail.com>
2024-06-03 10:30:48 +0300

    added functionality to delete job

--- view/cache/6853ffedd5efe19abf714e0cd2b332a4aa5082ba.php
+++ view/cache/6853ffedd5efe19abf714e0cd2b332a4aa5082ba.php
@@ -16,7 +16,7 @@
     </nav>
   </div><!-- End Page Title -->
 
-  <section class="section dashboard">
+  <section class="section dashboard" id="jobs-listing">
     <div class="card py-4  px-2 my-3">
       <div class="card-body">
         <div class="h5 text-secondary fw-bold">Job Title</div>
@@ -34,15 +34,82 @@
         <div><?php echo $job['requirements']; ?></div>
         <?php endif; ?>
         <hr>
-        <a href="/<?php echo e($appName); ?>/job-positions/listing/apply?id=<?php echo e($job['id']); ?>" class="btn btn-primary btn-sm">Apply Now</a>
-        <?php if($role == 'Administrator'): ?>
-        <a href="#" class="btn btn-success btn-sm">Update Job</a>
-        <a href="#" class="btn btn-danger btn-sm">Delete Job</a>
-        <?php endif; ?>
+        <form id="deleteForm" action="/<?php echo e($appName); ?>/jobs/delete/" method="POST">
+          <div class="d-flex">
+            <a href="/<?php echo e($appName); ?>/job-positions/listing/apply?id=<?php echo e($job['id']); ?>" class="btn btn-primary btn-sm me-2">Apply Now</a>
+            <?php if($role == 'Administrator'): ?>
+            <input type="hidden" name="id" value="<?php echo e($job['id']); ?>">
+            <button type="button" class="btn btn-danger btn-sm" id="remove-btn" data-toggle="modal" data-target="#confirmDeleteModal">Delete Job</button>
+            <?php endif; ?>
+
+          </div>
+        </form>
       </div>
     </div>
   </section>
 
+  <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-dark" id="confirmDeleteModalLabel">Confirm Your Action</h5>
+        </div>
+        <div class="modal-body">
+          <h6 class="text-dark">Are you sure you want to remove this job?</h6>
+          <div class="alert alert-warning">Note that this will also delete applications, shortlisted candidates, and all results associated with this job.</div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-sm" id="cancel-btn" data-dismiss="modal">Cancel</button>
+          <button type="button" id="confirmDeleteBtn" class="btn btn-danger btn-sm">Yes, Remove Job</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </main><!-- End #main -->
 
 <?php echo $__env->make('partials/footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+
+<script>
+  $(document).ready(function() {
+    var removeUrl;
+
+    $('#jobs-listing').on('click', '#remove-btn', function(event) {
+      event.preventDefault();
+      $('#confirmDeleteModal').modal('show');
+      removeUrl = $('#deleteForm').attr('action');
+    });
+
+    $('#confirmDeleteBtn').click(function() {
+      $.ajax({
+        url: removeUrl,
+        type: 'POST',
+        data: $('#deleteForm').serialize(),
+        success: function(response) {
+          Toastify({
+            text: response.message || 'Item removed successfully',
+            duration: 2000,
+            gravity: 'bottom',
+            backgroundColor: '#161b22',
+          }).showToast();
+
+          setTimeout(function() {
+            window.location.href = '/<?php echo e($appName); ?>/job-positions/listing/';
+          }, 2300);
+        },
+        error: function(xhr, status, error) {
+          console.error(xhr.responseText);
+        }
+      });
+      $('#confirmDeleteModal').modal('hide');
+    });
+
+    $('#cancel-btn').click(function() {
+      $('#confirmDeleteModal').modal('hide');
+    });
+
+    $('#confirmDeleteModal').on('hidden.bs.modal', function() {
+      $('#deleteForm').off('submit');
+    });
+  });
+</script>
--- view/templates/jobDetails.blade.php
+++ view/templates/jobDetails.blade.php
@@ -16,7 +16,7 @@
     </nav>
   </div><!-- End Page Title -->
 
-  <section class="section dashboard">
+  <section class="section dashboard" id="jobs-listing">
     <div class="card py-4  px-2 my-3">
       <div class="card-body">
         <div class="h5 text-secondary fw-bold">Job Title</div>
@@ -34,15 +34,82 @@
         <div>{!!$job['requirements']!!}</div>
         @endif
         <hr>
-        <a href="/{{$appName}}/job-positions/listing/apply?id={{$job['id']}}" class="btn btn-primary btn-sm">Apply Now</a>
-        @if($role == 'Administrator')
-        <a href="#" class="btn btn-success btn-sm">Update Job</a>
-        <a href="#" class="btn btn-danger btn-sm">Delete Job</a>
-        @endif
+        <form id="deleteForm" action="/{{$appName}}/jobs/delete/" method="POST">
+          <div class="d-flex">
+            <a href="/{{$appName}}/job-positions/listing/apply?id={{$job['id']}}" class="btn btn-primary btn-sm me-2">Apply Now</a>
+            @if($role == 'Administrator')
+            <input type="hidden" name="id" value="{{$job['id']}}">
+            <button type="button" class="btn btn-danger btn-sm" id="remove-btn" data-toggle="modal" data-target="#confirmDeleteModal">Delete Job</button>
+            @endif
+
+          </div>
+        </form>
       </div>
     </div>
   </section>
 
+  <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-dark" id="confirmDeleteModalLabel">Confirm Your Action</h5>
+        </div>
+        <div class="modal-body">
+          <h6 class="text-dark">Are you sure you want to remove this job?</h6>
+          <div class="alert alert-warning">Note that this will also delete applications, shortlisted candidates, and all results associated with this job.</div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-sm" id="cancel-btn" data-dismiss="modal">Cancel</button>
+          <button type="button" id="confirmDeleteBtn" class="btn btn-danger btn-sm">Yes, Remove Job</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </main><!-- End #main -->
 
 @include('partials/footer')
+
+<script>
+  $(document).ready(function() {
+    var removeUrl;
+
+    $('#jobs-listing').on('click', '#remove-btn', function(event) {
+      event.preventDefault();
+      $('#confirmDeleteModal').modal('show');
+      removeUrl = $('#deleteForm').attr('action');
+    });
+
+    $('#confirmDeleteBtn').click(function() {
+      $.ajax({
+        url: removeUrl,
+        type: 'POST',
+        data: $('#deleteForm').serialize(),
+        success: function(response) {
+          Toastify({
+            text: response.message || 'Item removed successfully',
+            duration: 2000,
+            gravity: 'bottom',
+            backgroundColor: '#161b22',
+          }).showToast();
+
+          setTimeout(function() {
+            window.location.href = '/{{$appName}}/job-positions/listing/';
+          }, 2300);
+        },
+        error: function(xhr, status, error) {
+          console.error(xhr.responseText);
+        }
+      });
+      $('#confirmDeleteModal').modal('hide');
+    });
+
+    $('#cancel-btn').click(function() {
+      $('#confirmDeleteModal').modal('hide');
+    });
+
+    $('#confirmDeleteModal').on('hidden.bs.modal', function() {
+      $('#deleteForm').off('submit');
+    });
+  });
+</script>
